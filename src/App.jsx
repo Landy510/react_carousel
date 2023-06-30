ad68a2dfb63235a0af129ba084bc672b7d119764
Landy510 <randy.chen@tpisoftware.com>
2023-06-30 15:56:27 +0800

    feat: carousel function 1. add - users can change the displayed images via pressing prev or next button

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+import './App.css'
+
+const source = [
+  {
+    id: 0,
+    bgc: 'rgb(255, 10, 0)'
+  },
+  {
+    id: 1,
+    bgc: 'rgb(255, 50, 0)'
+  },
+  {
+    id: 2,
+    bgc: 'rgb(255, 150, 0)'
+  },
+  {
+    id: 3,
+    bgc: 'rgb(255, 255, 50)'
+  }
+]
+
+function App() {
+  const [now, setNow] = useState(0);
+  
+  const allImages = () => {
+    const arr = [];
+    const total = source.length;
+    let count;
+    while(total > 0 && arr.length < 5 + 4) {
+      count = Math.floor(arr.length / total);
+
+      for(let i = 0; i < total; i++) {
+        arr.push({id: count + '-' + i, bgc: source[i].bgc})
+      }
+    }
+
+    return arr;
+  }
+
+  const showImages = () => {
+    const start = now - 4;
+    return allImages().slice(start).concat(allImages().slice(0, start));
+  }
+
+  function handleChange(index) {
+    let limit = allImages().length - 1;
+    setNow(index < 0 ? limit : index > limit ? 0 : index);
+  }
+
+  return (
+    <>
+      <div style={{
+        textAlign: 'center'
+      }}>
+        <button onClick={() => handleChange(now - 1)}>Prev</button>
+        <button onClick={() => handleChange(now + 1)}>Next</button>
+      </div>
+      <div 
+        className="container"
+      >
+        <div className="swiper-wrapper">
+          {
+            showImages().map(item => {
+              return (
+                <div 
+                  className='page' 
+                  key={item.id}
+                  style={{
+                    backgroundColor: item.bgc
+                  }}
+                >
+                  {item.id}
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
