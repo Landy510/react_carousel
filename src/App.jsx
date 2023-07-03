521f1b6d1d0fb6a3252c0c7589c31a4b20201344
Landy510 <randy.chen@tpisoftware.com>
2023-07-03 11:03:03 +0800

    feat: add animation feature 1. add - users can see slides swiping via dragging the container 2. add - make the swiping animation be continuous

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 const source = [
@@ -26,6 +26,14 @@ const source = [
 
 function App() {
   const [now, setNow] = useState(0);
+
+  const containerRef = useRef(null);
+  const isDrag = useRef(false);
+  const startX = useRef(0);
+  const startScrollLeft = useRef(0);
+  const firstChildWidth = useRef(0);
+
+  const directionRef = useRef('');
   
   const allImages = () => {
     const arr = [];
@@ -48,21 +56,77 @@ function App() {
     return allImages().slice(start).concat(allImages().slice(0, start));
   }
 
-  function handleChange(index) {
+  function handleChange(index, direction) {
+    directionRef.current = direction;
     let limit = allImages().length - 1;
     setNow(index < 0 ? limit : index > limit ? 0 : index);
+    // containerRef.current.scrollLeft += direction === 'prev' ? -firstChildWidth.current : +firstChildWidth.current
   }
+
+  const dragStart = (e) => {
+    isDrag.current = true;
+    containerRef.current.classList.add('dragging');
+
+    startX.current = e.pageX;
+    startScrollLeft.current = containerRef.current.scrollLeft;
+  }
+
+  const dragging = (e) => {
+    if(!isDrag.current) return;
+    containerRef.current.scrollLeft = startScrollLeft.current - (e.pageX - startX.current);
+  }
+
+  const dragStop = () => {
+    isDrag.current = false;
+    containerRef.current.classList.remove('dragging');
+  }
+
+  const infiniteScroll = () => {
+    console.log('Hello')
+    if(containerRef.current.scrollLeft === 0) {
+      console.log("you've reached the left end");
+      containerRef.current.classList.add('no-transition');
+      containerRef.current.scrollLeft = containerRef.current.scrollWidth - ( 2 * containerRef.current.offsetWidth );
+      containerRef.current.classList.remove('no-transition');
+    }
+    else if(Math.ceil(containerRef.current.scrollLeft) === (containerRef.current.scrollWidth - containerRef.current.offsetWidth)) {
+      console.log("you've reached the right end");
+      containerRef.current.classList.add('no-transition');
+      containerRef.current.scrollLeft = containerRef.current.offsetWidth;
+      containerRef.current.classList.remove('no-transition');
+    }
+  }
+
+  useEffect(() => {
+    containerRef.current.addEventListener('mousedown', dragStart)
+    containerRef.current.addEventListener('mousemove', dragging)
+    containerRef.current.addEventListener('scroll', infiniteScroll)
+    document.addEventListener('mouseup', dragStop)
+    firstChildWidth.current = containerRef.current.children[0].children[0].offsetWidth;
+
+    return () => {
+      containerRef.current.removeEventListener('mousemove', dragging);
+      containerRef.current.removeEventListener('mousedown', dragStart);
+      document.addEventListener('mouseup', dragStop);
+    }
+  }, [])
+
+  useEffect(() => {
+    containerRef.current.scrollLeft += directionRef.current === 'prev' ? -firstChildWidth.current : +firstChildWidth.current
+  }, [now])
 
   return (
     <>
       <div style={{
         textAlign: 'center'
       }}>
-        <button onClick={() => handleChange(now - 1)}>Prev</button>
-        <button onClick={() => handleChange(now + 1)}>Next</button>
+        <button onClick={() => handleChange(now - 1, 'prev')} >Prev</button>
+        <button onClick={() => handleChange(now + 1, 'next')}>Next</button>
       </div>
+
       <div 
         className="container"
+        ref={containerRef}
       >
         <div className="swiper-wrapper">
           {
@@ -71,12 +135,13 @@ function App() {
                 <div 
                   className='page' 
                   key={item.id}
-                  // style={{
-                  //   backgroundColor: item.bgc
-                  // }}
                 >
-                  {/* {item.id} */}
-                  <img src={item.src} alt="" />
+                  <p>{item.id}</p>
+                  <img 
+                    src={item.src} 
+                    alt="" 
+                    draggable={false}
+                  />
                 </div>
               )
             })
